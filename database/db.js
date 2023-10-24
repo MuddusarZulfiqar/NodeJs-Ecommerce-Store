430691707c87087201e98f9782f4be309c41efa3
faizan-purelogics <122025996+faizan-purelogics@users.noreply.github.com>
2023-10-24 15:27:58 +0500

    Minor changes and readme added

--- database/db.js
+++ database/db.js
@@ -1,12 +1,14 @@
-const mongoose = require('mongoose')
-const dataBaseConnect = function(){
-    const URI = process.env.DB_URL;
-    mongoose.connect(URI, {
-        useNewUrlParser: true, 
-        // useUnifiedTopology: true 
-        }).then(()=>{
-            console.log('Connected to ' + URI);
+const mongoose = require("mongoose");
+const dataBaseConnect = function () {
+  const URI = process.env.DB_URL;
+  mongoose
+    .connect(URI, {
+      useNewUrlParser: true,
+      // useUnifiedTopology: true
+    })
+    .then(() => {
+      console.log("Connected to MongoDB Atlas");
     });
-}
+};
 
-module.exports = dataBaseConnect
+module.exports = dataBaseConnect;
