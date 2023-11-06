d4dcdafcb1addc85bb82d2e2f821c2e2dc171700
Muddusar <muddusar.zulfiqar@purelogics.net>
2023-11-06 19:12:40 +0500

    allow cors

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: [
-      "http://localhost:8080/",
+      "http://localhost:8080",
       "https://nodejs-ecommerce-store-production.up.railway.app/",
       "http://192.168.5.76:8080",
       "http://192.168.5.76",
