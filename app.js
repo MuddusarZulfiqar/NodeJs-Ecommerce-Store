53612e8ae6d64c509f20eec647c6a43a20ace0cb
Muddusar <muddusar.zulfiqar@purelogics.net>
2023-11-06 18:54:15 +0500

    allow cors

--- app.js
+++ app.js
@@ -35,6 +35,8 @@ app.use(
     origin: [
       "http://localhost:8080/",
       "https://nodejs-ecommerce-store-production.up.railway.app/",
+      "http://192.168.5.76:8080/",
+      "http://192.168.5.76",
     ],
     credentials: true,
   })
